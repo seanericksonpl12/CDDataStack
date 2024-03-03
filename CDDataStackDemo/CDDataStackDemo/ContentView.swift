f8a87c0ca39db482dfbfc90684ebafaace838c73
Sean Erickson <sean.erickson@willowtreeapps.com>
2024-03-03 13:18:07 -0500

    Update example proj

--- CDDataStackDemo/CDDataStackDemo/ContentView.swift
+++ CDDataStackDemo/CDDataStackDemo/ContentView.swift
@@ -8,12 +8,39 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var viewModel: ViewModel = ViewModel(position: nil)
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("User Position: \(viewModel.position.x), \(viewModel.position.y)")
+                .padding()
+            Button {
+                viewModel.position.y += 1
+            } label: {
+                Image(systemName: "chevron.up")
+                    .imageScale(.large)
+            }
+            HStack {
+                Button {
+                    viewModel.position.x -= 1
+                } label: {
+                    Image(systemName: "chevron.left")
+                        .imageScale(.large)
+                        .padding()
+                }
+                Button {
+                    viewModel.position.x += 1
+                } label: {
+                    Image(systemName: "chevron.right")
+                        .imageScale(.large)
+                        .padding()
+                }
+            }
+            Button {
+                viewModel.position.y -= 1
+            } label: {
+                Image(systemName: "chevron.down")
+                    .imageScale(.large)
+            }
         }
         .padding()
     }
--- CDDataStackDemo/CDDataStackDemo/UserModel.swift
+++ CDDataStackDemo/CDDataStackDemo/UserModel.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+import CDDataStack
+import SwiftUI
+
+@Observable class ViewModel {
+    
+    var position: CGPoint = CGPoint(x: UserModel.shared.x, y: UserModel.shared.y) {
+        didSet {
+            UserModel.shared.x = Int(position.x)
+            UserModel.shared.y = Int(position.y)
+        }
+    }
+}
+
+class UserModel: CDAutoModel {
+    
+    static var shared: UserModel = UserModel()
+    
+    @AutoSave var x: Int = 0
+    @AutoSave var y: Int = 0
+}
